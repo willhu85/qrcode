058ccf5921273b1eda182f5f562aa3a18108de55
willhu <willhu@willhu-MacBook-Air.local>
2015-02-03 10:26:00 +0800

    qrcode

---
+++ myscript.js
@@ -0,0 +1,31 @@
+$(function(){
+	var qrcodeFun = function(url) {
+		$("#qrcode").html("");
+		jQuery('#qrcode').qrcode({
+			width: 240,
+			height: 240,
+			text	: url
+		});
+		//$("#url").html(url)
+	};
+	var currTitle, currUrl;
+	chrome.tabs.getSelected(function(tabs){
+		currUrl = tabs.url; 
+		qrcodeFun(currUrl)
+	})
+	var eleQrcodeBtn = $("#qrcodebtn");
+	var eleHttpinput = $("#httpinput");
+	eleQrcodeBtn.bind("click", function() {
+		if (eleHttpinput.val() == "" || eleHttpinput.val() == "不能输入空值") {
+			eleHttpinput.addClass("error").val("不能输入空值");
+			setTimeout(function() {
+				eleHttpinput.removeClass("error").val("");
+			}, 3000);
+		} else {
+			qrcodeFun(eleHttpinput.val());
+		}
+	});
+	eleHttpinput.bind("focus", function() {
+		eleHttpinput.removeClass("error").val("");
+	})
+})
